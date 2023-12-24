cd92869e385a632c6c2a3f21305ea5dea51fa72d
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2023-12-24 20:32:55 +0100

    Create function getComputerChoice

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # rock-paper-scissors
 Project: Rock Paper Scissors (The Odin Project)
+
+Creating a program with basic javascript, to play a Games of
+Rock Paper Scissors against the computer.
--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Hello world")
+
+
+// 3.
+// function getComputerChoice:
+// that will randomly return Rock Paper Scissors
+
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random() * 3);
+    let computerChoice;
+    console.log(randomNum);
+    switch(randomNum) {
+        case 0:
+            computerChoice = "rock";
+            break;
+        case 1:
+            computerChoice = "paper";
+            break;
+        case 2:
+            computerChoice = "scissors";
+            break; 
+    }
+    return computerChoice;
+}
+
+getComputerChoice();
+
+// 4.
+// function that plays a single round:
+// should take two parameters: 
+// -> "playerSelection"
+// -> "computerSelection"
+// should return a string that declares the winner of the round:
+// "You Lose! Paper beats Rock"
+
+
+// 5. 6.
+// function game():
+// previous function inside this to play 5 rounds in a row
+
+// use prompt() to get input from user
