600b0837d283d5eb488c0cd51a85134a5b54590e
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2023-12-25 05:03:57 +0100

    Refactor javascript code

--- script.js
+++ script.js
@@ -24,25 +24,23 @@ function game() {
     // function will play a round and return result
     function playRound(player, computer) {
         console.log("Player: " + player + "  ||  Computer: " + computer);
-
         playerWon = false;
-        computerWon = false
-        tie = false;
+        playAgain = false;
 
         // Player chooses rock
         if(player === "rock") {
             if(computer === "rock") {
-                tie = true;
+                playAgain = true;
                 return "It's a tie. Both have chosen Rock.";
             }
             else if(computer === "paper") {
-                computerWon = true;
+                playerWon = false;
                 return "You lose! Paper beats Rock."
             }
             else if(computer === "scissors") {
                 playerWon = true;
                 return "You win! Rock beats Scissors.";
-            }
+            } 
         }
         // Player chooses paper
         else if(player === "paper") {
@@ -51,18 +49,18 @@ function game() {
                 return "You win! Paper beats Rock.";
             }
             else if(computer === "paper") {
-                tie = true;
+                playAgain = true;
                 return "It's a tie. Both have chosen Paper.";
             }
             else if(computer === "scissors") {
-                computerWon = true;
+                playerWon = false;
                 return "You lose! Scissors beats Paper.";
             }       
         }
         // Player chooses scissors
         else if(player === "scissors") {
             if(computer === "rock") {
-                computerWon = true;
+                playerWon = false;
                 return "You lose! Scissors beats Rock";
             }
             else if(computer === "paper") {
@@ -70,16 +68,19 @@ function game() {
                 return "You win! Scissors beats Paper";
             }
             else if(computer === "scissors") {
-                tie = true;
+                playAgain = true;
                 return "It's a tie. Both have chosen Scissors.";
             }
+        } 
+        else {
+            playAgain = true;
+            console.log("Wrong Input. Please enter: Rock, Paper or Scissors.")
         }
     }
 
     let roundsPlayed = 0;
     let playerWon = false;
-    let computerWon = false
-    let tie = false;
+    let playAgain = false;
     let playerScore = 0;
     let computerScore = 0;
 
@@ -90,29 +91,29 @@ function game() {
 
         // 2. Player makes choice
         //let playerSelection = prompt("Enter Rock, Paper or Scissors").toLowerCase()
-        let playerSelection = "scissors";
+        let playerSelection = "rock";
 
         // 3. Round is played and result returned
         let resultGame = playRound(playerSelection, computerSelection);
         console.log(resultGame);
        
-        console.log("playerWon: " +  playerWon + "  ||  computerWon: " + computerWon + " ||  tie: " + tie);
 
         //  Check if player or computer has won the last round
-        if(tie === false) {
+        //console.log("playAgain: " + playAgain);
+        if(playAgain === false) {
             roundsPlayed += 1;
             if(playerWon === true) {
                 playerScore += 1;
             }
-            else if(computerWon === true) {
+            else {
                 computerScore += 1;
             }
-            else {console.log("This should not be happening!")}
         }
        
         // Check if 5 Rounds were played
         if(roundsPlayed === 5) {
             console.log(`Rounds played: ${roundsPlayed}.`);
+            console.log(`playerScore: ${playerScore}  ||  computerScore: ${computerScore}`);
             // Check Who has Won the Game
             if(playerScore > computerScore) {
                 console.log("Congratulation. You win the Game!");
@@ -130,3 +131,4 @@ function game() {
 
 game();
 
+console.log("Thanks for playing. The Odin Project.")
