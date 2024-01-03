96dabb5e05e68d2aa58aa5ed756f79e6b6d12095
MightyCharm <109044408+MightyCharm@users.noreply.github.com>
2024-01-03 13:41:23 +0100

    Refactor code again
    New functions and better UI

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-console.log("start")
+
+function playAgain() {
+    // set back to initial state
+    roundsPlayed = 0;
+    playerScore = 0;
+    computerScore = 0;
+    // clear text
+    divTextSelection.textContent = "";
+    divTextGame.textContent = "";
+    divTextResult.textContent = "";
+    divTextGameOver.textContent = "";
+    // activate buttons
+    btnRock.disabled = false;
+    btnPaper.disabled = false;
+    btnScissors.disabled = false;
+    // deactivate button
+    btnPlayAgain.disabled = true;
+
+}
+
 
 // function will return choice of computer
 function getComputerChoice() {
@@ -7,13 +26,13 @@ function getComputerChoice() {
     //console.log(randomNum);
     switch(randomNum) {
         case 0:
-            computerChoice = "rock";
+            computerChoice = "ROCK";
             break;
         case 1:
-            computerChoice = "paper";
+            computerChoice = "PAPER";
             break;
         case 2:
-            computerChoice = "scissors";
+            computerChoice = "SCISSORS";
             break; 
     }
     return computerChoice;
@@ -26,15 +45,15 @@ function playRound(player, computer) {
     let playAgain = false;
 
     // Player chooses rock
-    if(player === "rock") {
+    if(player === "ROCK") {
         switch(computer) {
-            case "rock":
+            case "ROCK":
                 playAgain = true;
                 break;
-            case "paper":
+            case "PAPER":
                 playerWon = false;
                 break;
-            case "scissors":
+            case "SCISSORS":
                 playerWon = true;
                 break;
         };
@@ -42,27 +61,27 @@ function playRound(player, computer) {
     // Player chooses paper
     else if(player === "paper") {
         switch(computer) {
-            case "rock":
+            case "ROCK":
                 playerWon = true;
                 break;
-            case "paper":
+            case "PAPER":
                 playAgain = true;
                 break;
-            case "scissors":
+            case "SCISSORS":
                 playerWon = false;
                 break;
         };       
     }
     // Player chooses scissors
-    else if(player === "scissors") {
+    else if(player === "SCISSORS") {
         switch(computer) {
-            case "rock":
+            case "ROCK":
                 playerWon = false;
                 break;
-            case "paper":
+            case "PAPER":
                 playerWon = true;
                 break;
-            case "scissors":
+            case "SCISSORS":
                 playAgain = true;
                 break;
         }
@@ -71,72 +90,95 @@ function playRound(player, computer) {
     return [playAgain, playerWon];
 }
 
-function game(player) {
-    
-    
-    if(roundsPlayed < 5) { 
-        playerSelection = player
-        let computerSelection = getComputerChoice();
 
-        // show on ui what player and computer choses
-         divTextSelection.textContent = `Player => "${playerSelection}" Computer => "${computerSelection}".`
-
-        // round is played and result returned
-        let resultGame = playRound(playerSelection, computerSelection);
-        let playAgain = resultGame[0]; // if true, it was a tie
-        let playerWon = resultGame[1]; // it true, player won the round
-        
-        //Check who has won the round
-        if(playAgain === false) { // it was no tie
+function getResult(resultRound) {
+        let playAgain = resultRound[0];
+        let playerWon = resultRound[1];
+        console.log("playAgain: " + playAgain + "   playerWon: " + playerWon );
+    
+        // check if round was not a tie
+        if(playAgain === false) {
             roundsPlayed += 1;
+            // check who won the round
             if(playerWon === true) {
                 playerScore += 1;
-                divTextGame.textContent = `Player won this round. Rounds played: ${roundsPlayed} || Player Score: ${playerScore} || Computerscore: ${computerScore}`
+                divTextGame.textContent = `Player won this round. Rounds played: ${roundsPlayed} || Player Score: ${playerScore} || Computerscore: ${computerScore}`;
             }
-            else { // computer won
+            else if(playerWon === false) {
                 computerScore += 1;
-                divTextGame.textContent = `Computer won this round. Rounds played: ${roundsPlayed} || Player Score: ${playerScore} || Computerscore: ${computerScore}`
-            }
-        } 
-        // if it was a tie
+                divTextGame.textContent = `Computer won this round. Rounds played: ${roundsPlayed} || Player Score: ${playerScore} || Computerscore: ${computerScore}`;
+            }    
+        }
+        // round was a tie
         else {
-            divTextGame.textContent = `It's a tie! Rounds played: ${roundsPlayed} || Playerscore: ${playerScore} || Computerscore: ${computerScore}`;
-        }       
-    } 
-    // 5 Rounds are played, Game Over
-    else {
-        // check who has won
-        if(playerScore > computerScore) {
-            divTextResult.textContent = `Congratulation. Player wins the Game ${playerScore} : ${computerScore}.`;
-        } 
-        else {
-            divTextResult.textContent = `Computer wins the game ${computerScore} : ${playerScore}.`
-        };    
+            divTextGame.textContent = `It' a tie. Round must be played again. Rounds played: ${roundsPlayed} || Player Score: ${playerScore} || Computerscore: ${computerScore}`;  
+        }
+   
+}
+
+function gameOver() {
+    if(playerScore > computerScore) {
+        divTextGameOver.textContent = "CONGRATULATION YOU WON THE GAME!";
+    } else {
+        divTextGameOver.textContent = "YOU LOSE! COMPUTER WON THE GAME!";
     }
+}
+
+
+function game(player) {
+    playerSelection = player
+    let computerSelection = getComputerChoice();
+
+    // show on ui what player and computer choses
+    divTextSelection.textContent = `Player: "${playerSelection}" || Computer: "${computerSelection}".`
+
+    // round is played and result returned
+    let resultRound = playRound(playerSelection, computerSelection);
+    getResult(resultRound);
+
+    // do something with checkResult
+    // check how many rounds are played
+
+    // less than 5 => continue
+    // 5 => announce winner
+    if(roundsPlayed >= 5) {
+        console.log("5 rounds played GAME OVER");
+
+        // deactivate buttons
+        btnRock.disabled = true;
+        btnPaper.disabled = true;
+        btnScissors.disabled = true;
+        // activate button
+        btnPlayAgain.disabled = false;
+        // announce winner
+        // active button play new game
+
+        // call function getResult
+        gameOver();
+    }  
 }
 
 
 let roundsPlayed = 0;
 let playerScore = 0;
 let computerScore = 0;
-const boxBtn = document.querySelector(".box-btn");
+const btnRock = document.querySelector("#btnRock");
+const btnPaper = document.querySelector("#btnPaper");
+const btnScissors = document.querySelector("#btnScissors");
 let divTextSelection = document.querySelector("#divSelection");
 let divTextGame = document.querySelector("#divGame");
 let divTextResult = document.querySelector("#divResult");
+let divTextGameOver = document.querySelector("#divGameOver");
+
+
+btnPlayAgain.disabled = true;
 
 function callGame(e) {
     // e.target.value = player rock/paper/scissors
     game(e.target.value);
-}
-//console.log("Thanks for playing. The Odin Project.")
-boxBtn.addEventListener("click", callGame);
+};
 
-
-
-
-
-// using bubbling to check which button was pressed
-//boxBtn.addEventListener("click", (e) => {
-//    game(e.target.value); // value (rock,paper, scissors) of clicked btn
-//})
-//game();
+btnRock.addEventListener("click", callGame);
+btnPaper.addEventListener("click", callGame);
+btnScissors.addEventListener("click", callGame);
+btnPlayAgain.addEventListener("click", playAgain);
